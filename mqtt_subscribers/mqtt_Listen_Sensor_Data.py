602df3513fac45a63a99c2512fe9223f0e080c8b
Oscar Macdonald (1000021598) <MACDO5@student.op.ac.nz>
2017-08-17 10:35:18 +1200

    reformatted mqtt_Listener_Sensor_Data to PEP 8.

--- mqtt_subscribers/mqtt_Listen_Sensor_Data.py
+++ mqtt_subscribers/mqtt_Listen_Sensor_Data.py
@@ -1,5 +1,5 @@
 #------------------------------------------
-#--- Author: Pradeep Singh
+#--- Original Author: Pradeep Singh
 #--- Date: 20th January 2017
 #--- Version: 1.0
 #--- Python Ver: 2.7
@@ -18,33 +18,35 @@ MQTT_Port = 1883
 Keep_Alive_Interval = 45
 MQTT_Topic = "application/+/node/+/rx" # display only the RX payloads for nodes
 
-#MongoDB settings
+# MongoDB settings
 # from http://api.mongodb.com/python/current/tutorial.html
 client = MongoClient()
 db = client.duniot_database
 mqtt_collection = db.duniot_collection
 
-#Subscribe to all Sensors at Base Topic
+# Subscribe to all Sensors at Base Topic
 def on_connect(mqttc, mosq, obj, rc):
-	mqttc.subscribe(MQTT_Topic, 0)
+    mqttc.subscribe(MQTT_Topic, 0)
 
-#Save Data into DB Table
+
+# Save Data into DB Table
 def on_message(mosq, obj, msg):
-	# This is the Master Call for saving MQTT Data into DB
-	# For details of "sensor_Data_Handler" function please refer "sensor_data_to_db.py"
-	print("MQTT Data Received...")
-	print("MQTT Topic: " + msg.topic)  
-	print(msg.payload)
-	new_mqtt_data = json.loads(msg.payload)
-	print("time:")
-	print(new_mqtt_data['rxInfo'][0]['time'])
-	# from https://stackoverflow.com/questions/127803
-	# Take the string for 'time' and convert into ISO-datetime format 8601DZ
-	new_mqtt_data['rxInfo'][0]['time'] = datetime.datetime.strptime(new_mqtt_data['rxInfo'][0]['time'], "%Y-%m-%dT%H:%M:%S.%fZ")
-	#Add the MQTT data to MongoDB
-	print("inserting into duniot_database.mqtt_collection")
-	new_entry_id = mqtt_collection.insert_one(new_mqtt_data).inserted_id
-	print("Success. Entry ID is " + str(new_entry_id))
+    # This is the Master Call for saving MQTT Data into DB
+    # For details of "sensor_Data_Handler" function please refer "sensor_data_to_db.py"
+    print("MQTT Data Received...")
+    print("MQTT Topic: " + msg.topic)
+    print(msg.payload)
+    new_mqtt_data = json.loads(msg.payload)
+    print("time:")
+    print(new_mqtt_data['rxInfo'][0]['time'])
+    # from https://stackoverflow.com/questions/127803
+    # Take the string for 'time' and convert into ISO-datetime format 8601DZ
+    new_mqtt_data['rxInfo'][0]['time'] = datetime.datetime.strptime(new_mqtt_data['rxInfo'][0]['time'], "%Y-%m-%dT%H:%M:%S.%fZ")
+    # Add the MQTT data to MongoDB
+    print("inserting into duniot_database.mqtt_collection")
+    new_entry_id = mqtt_collection.insert_one(new_mqtt_data).inserted_id
+    print("Success. Entry ID is " + str(new_entry_id))
+
 
 def on_subscribe(mosq, obj, mid, granted_qos):
     pass
@@ -54,7 +56,6 @@ print("print connecting")
 # Assign event callbacks
 mqttc.on_message = on_message
 mqttc.on_connect = on_connect
-#mqttc.on_subscribe = on_subscribe
 mqttc.subscribe("#")
 print("subscribed")
 # Connect
